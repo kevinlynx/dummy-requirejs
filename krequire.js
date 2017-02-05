e1aa71e0fe38d8299e58fc7649f107664e9d172c
Kevin Lynx <kevinlynx@gmail.com>
2017-02-05 16:48:31 +0800

    change Module member functions to prototype

--- krequire.js
+++ krequire.js
@@ -46,118 +46,119 @@
         mod.listeners = []
         mod.exports = {}
         mod.status = STATUS.UNFETCH
-
-        // load all dependent modules recursively but their factory not executed
-        mod.load = function() {
-            console.log('to load mod: ', mod.id)
-            if (mod.status == STATUS.FETCHING) return
-            if (mod.status == STATUS.UNFETCH) {
-                return mod.fetch()
-            }
-            mod.status = STATUS.LOADING
-            mod.remain = mod.deps.length
-            function callback() {
-                mod.remain--
-                if (mod.remain === 0) {
-                    mod.onload()
-                }
-            }
-            each(mod.deps, function (dep) {
-                console.log('load dep:', dep)
-                var m = getModule(dep)     
-                if (m.status >= STATUS.LOADED || m.status == STATUS.LOADING) {
-                    mod.remain--
-                    return
-                }
-                m.listeners.push(callback)
-                if (m.status < STATUS.LOADING) {
-                    m.load()
-                }
-            })
-            if (mod.remain == 0) {
+    }
+    
+    // load all dependent modules recursively but their factory not executed
+    Module.prototype.load = function() {
+        var mod = this
+        console.log('to load mod: ', mod.id)
+        if (mod.status == STATUS.FETCHING) return
+        if (mod.status == STATUS.UNFETCH) {
+            return mod.fetch()
+        }
+        mod.status = STATUS.LOADING
+        mod.remain = mod.deps.length
+        function callback() {
+            mod.remain--
+            if (mod.remain === 0) {
                 mod.onload()
             }
         }
-
-        // execute this and its deps
-        mod.exec = function() {
-            var mod = this
-            console.log('execute module:', mod.id)
-            if (mod.status >= STATUS.EXECUTED) { return mod.exports }
-            var args = mod.getDepsExport()
-            console.log('apply module factory:', mod.id)
-            var ret = mod.factory.apply(null, args)
-            mod.exports = ret 
-            mod.status = STATUS.EXECUTED
-            return mod.exports
-        }
-        
-        mod.getDepsExport = function() {
-            var mod = this
-            var exports = []
-            var deps = mod.deps
-            var argsLen = mod.factory.length < deps.length ? mod.factory.length : deps.length
-            for (var i = 0; i < argsLen; i++) {
-                exports.push(mod.require(deps[i]))
+        each(mod.deps, function (dep) {
+            console.log('load dep:', dep)
+            var m = getModule(dep)     
+            if (m.status >= STATUS.LOADED || m.status == STATUS.LOADING) {
+                mod.remain--
+                return
             }
-            return exports
-        }
-
-        mod.fetch = function() {
-            var mod = this
-            console.log('to fetch mod:', mod.id)
-            mod.status = STATUS.FETCHING
-
-            function onloadListener() {
-                var readyState = script.readyState;
-                if (typeof readyState === 'undefined' || /^(loaded|complete)$/.test(readyState)) {
-                    mod.status = STATUS.FETCHED
-                    console.log('mod fetched:', mod.id)
-                    mod.load()
-                }
+            m.listeners.push(callback)
+            if (m.status < STATUS.LOADING) {
+                m.load()
             }
+        })
+        if (mod.remain == 0) {
+            mod.onload()
+        }
+    }
 
-            var uri = mod.uri
-            var script = document.createElement('script')
+    // execute this and its deps
+    Module.prototype.exec = function() {
+        var mod = this
+        console.log('execute module:', mod.id)
+        if (mod.status >= STATUS.EXECUTED) { return mod.exports }
+        var args = mod.getDepsExport()
+        console.log('apply module factory:', mod.id)
+        var ret = mod.factory.apply(null, args)
+        mod.exports = ret 
+        mod.status = STATUS.EXECUTED
+        return mod.exports
+    }
+    
+    Module.prototype.getDepsExport = function() {
+        var mod = this
+        var exports = []
+        var deps = mod.deps
+        var argsLen = mod.factory.length < deps.length ? mod.factory.length : deps.length
+        for (var i = 0; i < argsLen; i++) {
+            exports.push(mod.require(deps[i]))
+        }
+        return exports
+    }
 
-            if (script.readyState) {
-                script.onreadystatechange = onloadListener
-            } else {
-                script.onload = onloadListener;
+    Module.prototype.fetch = function() {
+        var mod = this
+        console.log('to fetch mod:', mod.id)
+        mod.status = STATUS.FETCHING
+
+        function onloadListener() {
+            var readyState = script.readyState;
+            if (typeof readyState === 'undefined' || /^(loaded|complete)$/.test(readyState)) {
+                mod.status = STATUS.FETCHED
+                console.log('mod fetched:', mod.id)
+                mod.load()
             }
-
-            script.src = uri + '.js';
-            script.async = true;
-            appendScript(script);
         }
 
-        var headElement = document.getElementsByTagName('head')[0];
-        var baseElement = document.getElementsByTagName('base')[0];
+        var uri = mod.uri
+        var script = document.createElement('script')
 
-        if (baseElement) {
-            headElement = baseElement.parentNode;
-        }
-        
-        function appendScript(script) {
-            baseElement
-                ? headElement.insertBefore(script, baseElement)
-                : headElement.appendChild(script);
+        if (script.readyState) {
+            script.onreadystatechange = onloadListener
+        } else {
+            script.onload = onloadListener;
         }
 
-        mod.onload = function() {
-            var mod = this
-            mod.status = STATUS.LOADED
-            console.log('mod loaded, callback:', mod.id)
-            each(mod.listeners, function (listener) {
-                listener()
-            })
-            mod.callback && mod.callback()
-        }
+        script.src = uri + '.js';
+        script.async = true;
+        appendScript(script);
+    }
 
-        mod.require = function(dep) {
-            var mod = getModule(dep)
-            return mod.exec()
-        }
+    var headElement = document.getElementsByTagName('head')[0];
+    var baseElement = document.getElementsByTagName('base')[0];
+
+    if (baseElement) {
+        headElement = baseElement.parentNode;
+    }
+    
+    function appendScript(script) {
+        baseElement
+            ? headElement.insertBefore(script, baseElement)
+            : headElement.appendChild(script);
+    }
+
+    Module.prototype.onload = function() {
+        var mod = this
+        mod.status = STATUS.LOADED
+        console.log('mod loaded, callback:', mod.id)
+        each(mod.listeners, function (listener) {
+            listener()
+        })
+        mod.callback && mod.callback()
+    }
+
+    Module.prototype.require = function(dep) {
+        var mod = getModule(dep)
+        return mod.exec()
     }
 
     function require(deps, callback) {
